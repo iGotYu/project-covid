9fdab9b4bb81f6d73f639f87e15b2c667ce27ae8
Andrew Yu <Andy@Andrews-MacBook-Pro.local>
2021-04-05 10:51:37 -0700

    navbar

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,25 @@
 console.log('linked fosho');
+var options = {
+    option1: document.querySelector(‘#option1’),
+    option2: document.querySelector(‘#option2’),
+    option3: document.querySelector(‘#option3’)
+}
+// fetch api
+// find by zip
+// narrow range by option selected
+function getApi(options) {
+    var mapApi = `https://api.mapbox.com/geocoding/v5`
+}
+// mapboxgl.baseApiUrl = ’https://api.mapbox.com';
+document.addEventListener(‘click’, function(event) {
+    event.preventDefault();
+    console.log(‘clickkkkked’);
+})
+document.addEventListener(‘DOMContentLoaded’, function() {
+    var elems = document.querySelectorAll(‘select’);
+    var instances = M.FormSelect.init(elems, options);
+  });
+
+  $( document ).ready(function{
+    $(".dropdown-trigger").dropdown();
+  });
