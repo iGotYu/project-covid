f67ce41ac275252240d70cb23baafc162550d44c
Andrew Yu <igotyuu421@gmail.com>
2021-04-06 10:34:52 -0700

    pulling

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,27 @@
 console.log('linked fosho');
+
+
+$( document ).ready(function(){
+  $(".dropdown-trigger").dropdown();
+  $('.carousel').carousel();
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.dropdown-trigger');
+  var instances = M.Dropdown.init(elems, options);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.carousel');
+  var instances = M.Carousel.init(elems, options);
+});
+
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     var elems = document.querySelectorAll('.carousel');
+//     var instances = M.Carousel.init(elems, options);
+//   });
+
+  // $(document).ready(function(){
+  //   $('.carousel').carousel();
+  // });
