acb16a8eeed63d5fe8db2a15b4ed2987cd4510c3
Joy Rhee <joyfullyx@gmail.com>
2021-04-05 19:19:01 -0700

    loop through states to extract zips

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,5 @@
 var searchButton = document.querySelector('#searchBtn')
+var stateSearchButton = document.querySelector('#state-search-btn');
 
 var options = {
     option1: document.querySelector('#option1'),
@@ -26,27 +27,46 @@ function getApi(options) {
 // var locatorApi = `https://www.vaccinespotter.org/api/${locatorStates}`;
 
 // fetch api data for vaccine locators by state
+var stateInput = document.querySelector('#state')
 var searchInput = document.querySelector('#user-input')
+var zipCodeArr = new Array;
 
 // var searchLocation = 'WA';
 function fetchLocation(location) {
-    var locatorApi = `https://www.vaccinespotter.org/api/v0/states/${searchInput.value}.json`;
+    console.log(location);
+    var locatorApi = `https://www.vaccinespotter.org/api/v0/states/${location}.json`;
     fetch(locatorApi).then(function(data){
         return data.json()
     }).then(function(data){
+        var {name, address, city, state, postal_code, appointment_vaccine_types, appointments_available, appointments_available_2nd_dose_only, carries_vaccine, url} = data.features[0].properties;
         console.log(data);
+        // console.log(city);
+        
+        for (var i = 0; i < data.features.length; i++) {
+            var option = document.createElement('option');
+            var zipCode = document.createTextNode(data.features[i].properties.postal_code);
+            option.appendChild(zipCode);
+            searchInput.appendChild(option);
+            console.log(zipCode);
+        }
+
+        console.log({name, address, city, state, postal_code, appointment_vaccine_types, appointments_available, appointments_available_2nd_dose_only, carries_vaccine, url});
     })
 }
 
 
-searchButton.addEventListener('click', function(event) {    event.preventDefault();
-    console.log('clickkkkked');
+searchButton.addEventListener('click', function(event) {    
+    event.preventDefault();
+    // console.log('clickkkkked');
 
-    var location = searchInput.value;
-    console.log(location)
+    var location = stateInput.value;
+    console.log(location)  
+})
 
-    
+stateSearchButton.addEventListener('click', function(event) {
+    event.preventDefault();
 
+    var location = stateInput.value;
     fetchLocation(location);
 })
 
